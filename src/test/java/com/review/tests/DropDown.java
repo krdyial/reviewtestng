aab7ef8310b90a6f7dad035056d36bacfc8554d5
Alaettin <alaettinkaradayi@gmail.com>
2021-01-13 01:19:25 +0100

    good nigt github

---
+++ src/test/java/com/review/tests/DropDown.java
@@ -0,0 +1,42 @@
+package com.review.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropDown {
+//Create a class name : DropDown
+//Go to https://the-internet.herokuapp.com/dropdown
+//1.Select and Print Option 1 using index
+//2.Select and Print Option 2 by value
+//3.Select and Print default value by visible text
+//4.Print all dropdown values
+//5.Find the size of the dropdown, Print true if there are 4 elements in the dropdown. Print false otherwise.
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+
+    }
+
+    @Test
+    public void dropdownTest(){
+        WebElement dropdowneElement=driver.findElement(By.id("dropdown"));
+        Select select= new Select(dropdowneElement);
+        select.selectByIndex(1);
+
+
+    }
+}
