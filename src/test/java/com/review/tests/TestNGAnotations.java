c2ffc65445fe3b72a2542eec7f2f04ad05ac9e4c
Alaettin <alaettinkaradayi@gmail.com>
2021-01-13 01:08:12 +0100

    priority added

--- src/test/java/com/review/tests/TestNGAnotations.java
+++ src/test/java/com/review/tests/TestNGAnotations.java
@@ -1,9 +1,11 @@
 package com.review.tests;
 
-import org.junit.Ignore;
-import org.junit.Test;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
 
 public class TestNGAnotations {
 
@@ -17,8 +19,8 @@ public class TestNGAnotations {
         System.out.println("This is after method");
     }
 
-    @Test
-    public void test1(){
+    @Test(priority = 4-10)
+    public void test5(){
         System.out.println("test1");
     }
     @Test
@@ -26,11 +28,11 @@ public class TestNGAnotations {
     public void test2(){
         System.out.println("test2");
     }
-    @Test
+    @Test(priority = 6)
     public void test3(){
         System.out.println("test3");
     }
-    @Test
+    @Test(priority = 1)
     public void test4(){
         System.out.println("test4");
     }
